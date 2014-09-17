328714d399eba4cf9ee2979a127a8a2bbf620926
wsizoo <will.sizoo@appfolio.com>
2014-09-16 17:37:08 -0700

    add too lazy condition to help

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -8,10 +8,10 @@ module StoriesHelper
   end
 
   def format_case_link_home(story)
-    if story.case_link != nil
-      link_to story.case_link, story.case_link, :target => '_blank'
-    else
+    if story.case_link == 'http://' || story.case_link.blank?
       'I was too lazy to add the case link.'
+    else
+      link_to story.case_link, story.case_link, :target => '_blank'
     end
   end
 
@@ -24,10 +24,10 @@ module StoriesHelper
   end
 
   def format_website_link_home(story)
-    if story != nil
-      link_to story.website, story.website, :target => '_blank'
-    else
+    if story.website == 'http://' || story.website.blank?
       'I was too lazy to add the website url.'
+    else
+      link_to story.website, story.website, :target => '_blank'
     end
   end
 end
